a76a79a449696efa226b60ce910bcaa56e707494
Adam <adam_kirk_msc@hotmail.com>
2019-06-11 19:11:46 +0100

    made major updates to the CSS and Javascript

--- main.js
+++ main.js
@@ -47,6 +47,72 @@
 		}
 	}
 
+
+
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<< Start toggle animation code >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// ----------------- This function shows a hidden element ------------------------------------------
+
+let showElement = function (elem) {
+    console.log(elem)
+	let getHeight = function () {
+		elem.style.display = 'block';              // Make it visible
+        let height = elem.scrollHeight -20 + 'px'; // Get it's height
+        elem.style.display = '';                   // Hide it again
+        return height;                             // Return the height
+    };
+	
+	let height = getHeight();                  // Get the height from the above function
+    elem.classList.add('is-visible');          // Make the element visible by adding a class
+    elem.style.height = height;                // Update the max-height
+	
+	window.setTimeout(function () {elem.style.height = '' }, 350); // Remove height after animate to scale correctly
+};
+
+// ----------------- This function hides a visible element ------------------------------------------
+
+const hideElement = function (elem) {
+	
+	elem.style.height = elem.scrollHeight - 20 + 'px';         // Returns the size of the elment with px at the end
+	window.setTimeout(function () {elem.style.height = '0'}, 1);               // Sets the height to 0 to skrink it
+	window.setTimeout(function () {elem.classList.remove('is-visible') }, 350); // Hides the elemnt by removing a class
+
+};
+
+// ----------------- This function is a toggle to hide and show elements ----------------------------
+
+const toggle = function (elem, timing) {
+	// If the element is visible, hide it
+	if (elem.classList.contains('is-visible')) { // If element has a class of is-visible, do this
+		hideElement(elem);                       // Run function hideElement
+		return;                                  // Exit this function
+    }
+    showElement(elem);                           // If it does not have element run function showElement
+};
+
+// ----------------- This function checks to see if the element clicked is toggle then runs the function toggle if true ----------------------------
+
+document.addEventListener('click', function (event) {
+	
+	if (!event.target.classList.contains('toggle')) return; // If element click does not have class of toggle then exit function
+	console.log(event)
+
+    let content = document.getElementById(event.target.attributes.href.value) // The item clicked should have a href with the toggle contents name
+	
+	if (!content) return;  // If not content exit function
+	toggle(content);       // run the toggle function on the content
+
+}, false);
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<< End toggle animation code >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+
+
+
 // -------------------- jQuery coding ------------------------
 let linkClosed = false;
 $('.navCase').animate({right:'-80%'},0);
